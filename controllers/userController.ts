c4ef54726d8781c8acde437a99cfe5636a5462cf
Houssam OUATMMANI <ouatmanihoussam@gmail.com>
2025-01-23 14:49:03 +0100

    FEAT: UPDATE USER PASSWORD

--- controllers/userController.ts
+++ controllers/userController.ts
@@ -1,5 +1,6 @@
 
 import type {Request, Response} from "express"
+import bcrypt from "bcrypt"
 import { UserModel } from "../models/userModel"
 
 export async function updateUsername(req: Request, res: Response) {
@@ -12,5 +13,23 @@ export async function updateUsername(req: Request, res: Response) {
     user.username = newUsername
     user.save()
     return res.status(200).json({message: "username updated!"})
+
+}
+
+export async function updatePassword(req: Request, res: Response) {
+    const {oldPassword, newPassword} = req.body
+     // @ts-ignore
+     const id = req.user.id
+     const user = await UserModel.findById(id)
+     if(!user) return res.status(404).json({message: "user Not found"})
+
+     const match = await bcrypt.compare(oldPassword, user.password)
+     if(!match) return res.status(401).json({message : "password incorrect"})
+     const hash = await bcrypt.hash(newPassword, 10)
+     user.password = hash
+     await user.save()
+     
+     return res.status(200).json({message: "password updated!"})
+
 
 }
--- routes/userRoutes.ts
+++ routes/userRoutes.ts
@@ -1,8 +1,11 @@
 import {Router} from "express"
 import { authMiddleware } from "../middlewares/authMiddleware"
-import { updateUsername } from "../controllers/userController"
+import { updatePassword, updateUsername } from "../controllers/userController"
 
 export const userRouter = Router()
 
 // @ts-ignore
 userRouter.put("/username/update", authMiddleware, updateUsername)
+
+// @ts-ignore
+userRouter.put("/password/update", authMiddleware, updatePassword)
